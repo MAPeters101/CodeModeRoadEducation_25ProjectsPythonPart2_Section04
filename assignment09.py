2d3ed0440b8541360f4c45f37acc6446a8b8720c
Mark Peters <mapeters101@earthlink.net>
2025-01-10 23:11:31 -0500

    Remove directive comments for assignment09.

--- assignment09.py
+++ assignment09.py
@@ -1,28 +1,17 @@
 print("Welcome to the Phonebook!\n")
-
-# Initialize your phonebook dictionary
 phonebook = {}
 
-# User Input Loop
 while True:
-  # Ask the user to enter a name (or 'STOP' to end - in which case break out of the loop)
   name = input("Please enter a name (STOP to quit): ")
   if name.upper() == "STOP":
     break
-
-  # Ask the user to enter a phone number (or 'STOP' to end - in which case break out of the loop)
   phone = input("Please enter the number (STOP to quit): ")
   if phone.upper() == "STOP":
     break
-
-  # Append the new key-value pair (name & number) to the dictionary
   phonebook[name] = phone
 
-# Outisde the first loop
-# Sort the dictionary - Replace 'dictionaryName' with what your phonebook dictionary is named
 sortedPhonebook = dict(sorted(phonebook.items()))
 
-# User Options Loop
 while True:
   print("\nUser Options:")
   print("1. Look Up a Name")
@@ -32,9 +21,6 @@ while True:
 
   choice = input("Enter your choice (1/2/3): ")
 
-  # If choice is 1, ask the user to enter a name
-  # If the name is in the dictionary, print the phone number
-  # If the name is not in the dictionary, print 'Name not found'
   if choice == "1":
     name = input("Please enter a name (STOP to quit): ")
     if name in sortedPhonebook:
@@ -42,10 +28,6 @@ while True:
     else:
       print("Name not found.")
 
-  # If choice is 2, ask the user to enter a name
-  # If the name is in the dictionary, ask the user to enter a new phone number
-  # If the name is not in the dictionary, print 'Name not found'
-  # Replace the old phone number with the new one
   if choice == "2":
     name = input("Please enter a name: ")
     if name in sortedPhonebook:
@@ -54,15 +36,12 @@ while True:
     else:
       print("Name not found.")
 
-  # If choice is 3, print the entire dictionary
   if choice == "3":
     print(sortedPhonebook)
 
-  # If choice is 4, break out of the loop
   if choice == "4":
     break
 
-  # Repeat or Quit
   repeat = input(
       "Do you want to perform another operation? (Enter 'STOP' to quit): "
   ).strip().lower()
