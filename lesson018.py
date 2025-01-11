20d34b7c1ec7a2146f391145a683596d29281ce4
Mark Peters <mapeters101@earthlink.net>
2025-01-10 21:36:44 -0500

    Get inout for user for lesson018 from 3:00.

--- lesson018.py
+++ lesson018.py
@@ -1,3 +1,12 @@
 videoGameScores = {"sam":9, "susan":88, "johnny":1000, "trevor":14, "annie":987}
 
 print(videoGameScores)
+
+choice = int(input("Do you want to: \n(1) Change the score of a player\n(2) Delete a player \n(3) Add a player\n"))
+
+while choice > 3 or choice < 1:
+    print("Invalid")
+    choice = int(input(
+        "Do you want to: \n(1) Change the score of a player\n(2) Delete a player \n(3) Add a player\n"))
+
+
