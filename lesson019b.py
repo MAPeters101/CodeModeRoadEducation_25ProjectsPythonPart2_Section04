cba36b55859ef26dd9c031402104048161761497
Mark Peters <mapeters101@earthlink.net>
2025-01-11 15:23:05 -0500

    Work on lesson019 fro 1:20 to 4:50.

---
+++ lesson019b.py
@@ -0,0 +1,4 @@
+contactList = {"Adam": {"email": "adam@gmail.com", "phone": "123-456-7890"},
+               "Anna": {"email": "anna@gmail.com", "phone": "321-654-0987"},
+               "Brandon": {"email": "brandon@gmail.com", "phone": "098-765-4321"},
+               "Bonny": {"email": "bonney@gmail.com", "phone": "890-567-1234"}}
