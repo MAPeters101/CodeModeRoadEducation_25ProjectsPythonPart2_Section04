ee5c02637b5bb6a08aba6612738e8fd49403f05c
Mark Peters <mapeters101@earthlink.net>
2025-01-10 21:49:58 -0500

    Complete work on lesson018 from 3:00.

--- lesson018.py
+++ lesson018.py
@@ -9,4 +9,25 @@ while choice > 3 or choice < 1:
     choice = int(input(
         "Do you want to: \n(1) Change the score of a player\n(2) Delete a player \n(3) Add a player\n"))
 
+if choice == 1:
+    changePlayer = input("Which player's score do you want to change: ")
+    if changePlayer in videoGameScores:
+        changeScore = int(input(f"What is {changePlayer}'s new score: "))
+        videoGameScores[changePlayer] = changeScore
+    else:
+        print("Invalid")
+
+if choice == 2:
+    remPlayer = input("Which player do you want to remove: ")
+    if remPlayer in videoGameScores:
+        del videoGameScores[remPlayer]
+    else:
+        print("Invalid")
+
+if choice == 3:
+    newPlayer = input("What's the name of the new player: ")
+    newScore = int(input(f"What is {newPlayer}'s score: "))
+    videoGameScores[newPlayer] = newScore
+
+print(videoGameScores)
 
