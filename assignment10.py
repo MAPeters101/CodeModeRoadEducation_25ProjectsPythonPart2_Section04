77a86bb2d0d33b3544717249938930889f348721
Mark Peters <mapeters101@earthlink.net>
2025-01-10 23:19:26 -0500

    Add pass statements to functions for assignment10.

--- assignment10.py
+++ assignment10.py
@@ -6,56 +6,56 @@ gamingLobby = {}
 # Make this function modify the current level that a player is
 # Remember, only positive level changes (>0) because nobody goes down a level
 def modifyLevel(playerName, levelChange):
+    pass
+
+    # Check if the player is in the gaming lobby first of all
 
 
-# Check if the player is in the gaming lobby first of all
+    # Then check if the level change is positive
 
 
-# Then check if the level change is positive
+    # If both of these conditions are true, then you can change the level
 
 
-# If both of these conditions are true, then you can change the level
-
-
-# Otherwise, print that either the player isn't in the lobby
-# Or a message stating that 'level change must be positive'
+    # Otherwise, print that either the player isn't in the lobby
+    # Or a message stating that 'level change must be positive'
 
 # Make this function add a player to the gaming lobby
 def addPlayer(playerName, playerLevel):
+    pass
+
+    # Check if the player is already in the lobby
+    # If they are, don't add the player
 
 
-# Check if the player is already in the lobby
-# If they are, don't add the player
+    # Then check if the playerLevel is above 0 (>0)
+    # Player levels can't be negative (it just doesn't make sense)
 
 
-# Then check if the playerLevel is above 0 (>0)
-# Player levels can't be negative (it just doesn't make sense)
-
-
-# If the player isn't in the lobby, and the level is valid,
-# add the player to the dictionary with along with their starting level
+    # If the player isn't in the lobby, and the level is valid,
+    # add the player to the dictionary with along with their starting level
 
 # Make this function remove a player to the gaming lobby
 def removePlayer(playerName):
+    pass
+
+    # Check if the player is in the lobby
 
 
-# Check if the player is in the lobby
+    # If so, remove the player from the lobby
 
 
-# If so, remove the player from the lobby
-
-
-# Otherwise, print that the player isn't in the lobby
+    # Otherwise, print that the player isn't in the lobby
 
 # THIS IS AN OPTIONAL FUNCTION (YOU DO NOT NEED TO INCLUDE IT)
 # If you don't include this function, comment out the header
 # Also, remove the section of code in the main code that mentions 'leaderboard'
 def leaderboard():
+    pass
 
-
-# To print the leaderboard, you will need to do extra research
-# On how to sort a dictionary based on the values
-# Or you can prove me wrong and come up with a solution with what we've       learned
+    # To print the leaderboard, you will need to do extra research
+    # On how to sort a dictionary based on the values
+    # Or you can prove me wrong and come up with a solution with what we've       learned
 
 
 # Main code
